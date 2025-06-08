b191eafeb4b33d6c2bf49c1285c07cf714c06de6
hoooonshub <yhn515@naver.com>
2025-06-08 21:02:10 +0900

    feat: cors 및 security 설정 파일 추가

---
+++ src/main/java/privatesns/capstone/common/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package privatesns.capstone.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("192.168.0.2") // 랩실 iptime
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
