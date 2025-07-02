26e9148c8f7a1f07b5d9064a8294fe279c4fdcec
hoooonshub <yhn515@naver.com>
2025-07-02 16:05:33 +0900

    feat: 500 에러 핸들러 추가

--- src/main/java/privatesns/capstone/core/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/privatesns/capstone/core/exception/advice/GlobalExceptionHandler.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import privatesns.capstone.core.exception.exception.BaseException;
 import privatesns.capstone.core.exception.exception.FileException;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
 @RestControllerAdvice
@@ -26,6 +29,7 @@ public class GlobalExceptionHandler {
             FileException.class,
             Exception.class
     })
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(Exception exception) {
         log.error("[Unexpected Exception] : exception={}, message={}",
                 exception.getClass().getName(),
